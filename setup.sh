802f27651e1914bebecfdd22c63463a172bd0776
Thorgrim Jansrud <48207719+thorgrimjansrud@users.noreply.github.com>
2023-03-21 08:15:33 +0100

    Add files via upload
    pull and init onboarding files

---
+++ setup.sh
@@ -0,0 +1,26 @@
+
+wget wget -O onboarding.sh https://raw.githubusercontent.com/Wago-Norge/wagono-mid-tig-stack/main/onboarding.sh --no-check-certificate
+wget wget -O influxdb-init.iql https://raw.githubusercontent.com/thorgrimjansrud/onboarding/main/influxdb-init.iql --no-check-certificate
+wget wget -O influxdb.conf https://raw.githubusercontent.com/thorgrimjansrud/onboarding/main/influxdb.conf --no-check-certificate
+wget wget -O telegraf.conf https://raw.githubusercontent.com/thorgrimjansrud/onboarding/main/telegraf.conf --no-check-certificate
+
+chmod +x onboarding.sh
+chmod +x influxdb-init.iql
+chmod +x influxdb.conf
+chmod +x telegraf.conf
+
+mv influxdb-init.iql /home/admin
+mv influxdb.conf /home/admin
+mv telegraf.conf /home/admin
+
+
+
+
+
+
+
+
+
+
+
+
