d087ff71756b794983207582ba28c361f28ab5cc
Thorgrim Jansrud <48207719+thorgrimjansrud@users.noreply.github.com>
2023-03-21 08:35:43 +0100

    fixed wget bug

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 
-wget wget -O onboarding.sh https://raw.githubusercontent.com/Wago-Norge/wagono-mid-tig-stack/main/onboarding.sh --no-check-certificate
-wget wget -O influxdb-init.iql https://raw.githubusercontent.com/Wago-Norge/wagono-mid-tig-stack/main/influx/influxdb-init.iql --no-check-certificate
-wget wget -O influxdb.conf https://raw.githubusercontent.com/Wago-Norge/wagono-mid-tig-stack/main/influx/influxdb.conf --no-check-certificate
-wget wget -O telegraf.conf https://raw.githubusercontent.com/Wago-Norge/wagono-mid-tig-stack/main/telegraf/telegraf.conf --no-check-certificate
+wget -O onboarding.sh https://raw.githubusercontent.com/Wago-Norge/wagono-mid-tig-stack/main/onboarding.sh --no-check-certificate
+wget -O influxdb-init.iql https://raw.githubusercontent.com/Wago-Norge/wagono-mid-tig-stack/main/influx/influxdb-init.iql --no-check-certificate
+wget -O influxdb.conf https://raw.githubusercontent.com/Wago-Norge/wagono-mid-tig-stack/main/influx/influxdb.conf --no-check-certificate
+wget -O telegraf.conf https://raw.githubusercontent.com/Wago-Norge/wagono-mid-tig-stack/main/telegraf/telegraf.conf --no-check-certificate
 
 chmod +x onboarding.sh
 chmod +x influxdb-init.iql
